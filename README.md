eaf31c19a23e9da333defb660078112e84e410d4
Sondre Tungesvik Njåstad <sn@effera.no>
2020-04-16 22:58:41 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # node-red-contrib-bluelinky
+[![npm](https://img.shields.io/npm/v/node-red-contrib-bluelinky.svg)](https://www.npmjs.com/package/bluelinky)
+[![Discord](https://img.shields.io/discord/652755205041029120)](https://discord.gg/HwnG8sY)
+
+![Sample](https://i.imgur.com/eUhFjxq.png)
+
+## Install
+```sh
+npm install node-red-contrib-bluelinky
+```
+
+## Documentation
+Checkout out the docs [bluelinky-docs](https://hacksore.github.io/bluelinky-docs/) for more info.
+
+## Supported Features
+- Lock
+- Unlock
+- Start (with climate control)
+- Stop
+- Status
+
+## Supported Regions
+| [Regions](https://github.com/Hacksore/bluelinky/wiki/Regions) | Current Status |
+|-----------|--------------|
+| USA       | Working      |
+| Canada    | Working      |
+| Europe    | Working      |
+| Korea     | ▯▯▯▯▯▯  |
+| Australia | sorry m8     |
+
+## Contributors
+
+[altagir](https://github.com/altagir) - Canada wizard 🧙‍♂️
+
+[SondreNjaastad](https://github.com/SondreNjaastad) - Europe wizard 🧙‍♂️
+
+[aleGuardiola](https://github.com/aleGuardiola) - Fixed generation one cars 😎
--- bluelinky.js
+++ bluelinky.js
@@ -1,25 +1,30 @@
 const BlueLinky = require('bluelinky');
 
-const client = new BlueLinky({
-    username: '',
-    password: '',
-    region: '',
-    pin: '',
-    deviceUuid: ''
-});
+let client;
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
 module.exports = function(RED) {
-    function GetVehicles(config) {
+    function GetVehicleStatus(config) {
         RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
         var node = this;
         node.on('input', async function(msg) {
             try{
                 await client.enterPin();
-                let cars = await client.getVehicles();
-                let status = await cars[0].status(false);
-                console.log(status);
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                let status = await car.status(true);
                 node.send({
-                    payload: status
+                    payload: {
+                        status,
+                        odometer: car.odometer,
+                        location: car.location
+                    }
                 });
             } catch (err){
                 node.send({
@@ -28,5 +33,154 @@ module.exports = function(RED) {
             }
         });
     }
-    RED.nodes.registerType("get-vehicles",GetVehicles);
+
+    function Unlock(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                await client.getVehicles();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                let result = await car.unlock();
+                node.send({
+                    payload: result
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function Location(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                await client.getVehicles();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                await car.status(true);
+                node.send({
+                    payload: car.location
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function Odometer(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                await client.getVehicles();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                await car.status(true);
+                node.send({
+                    payload: car.odometer
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function Start(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                let result = await car.start(msg.payload);
+                node.send({
+                    payload: result
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function Stop(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                let result = await car.stop(msg.payload);
+                node.send({
+                    payload: result
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function Lock(config) {
+        RED.nodes.createNode(this,config);
+        this.bluelinkyConfig = RED.nodes.getNode(config.bluelinky);
+        var node = this;
+        node.on('input', async function(msg) {
+            try{
+                await client.enterPin();
+                let car = await client.getVehicle(this.bluelinkyConfig.vin);
+                let result = await car.lock();
+                node.send({
+                    payload: result
+                });
+            } catch (err){
+                node.send({
+                    payload: err
+                })
+            }
+        });
+    }
+
+    function BluelinkyNode(n) {
+        RED.nodes.createNode(this,n);
+        this.username = n.username;
+        this.password = n.password;
+        this.region = n.region;
+        this.pin = n.pin;
+        this.deviceUuid = uuidv4();
+        this.vin = n.vin;
+
+        client = new BlueLinky({
+            username: this.username,
+            password: this.password,
+            region: this.region,
+            pin: this.pin,
+            deviceUuid: this.deviceUuid,
+        });
+    }
+
+    RED.nodes.registerType("bluelinky", BluelinkyNode);
+    RED.nodes.registerType("car-status", GetVehicleStatus);
+    RED.nodes.registerType("unlock-car", Unlock);
+    RED.nodes.registerType("lock-car", Lock);
+    RED.nodes.registerType("car-odometer", Odometer);
+    RED.nodes.registerType("car-location", Location);
+    RED.nodes.registerType("start-car", Start);
+    RED.nodes.registerType("stop-car", Stop);
 }
